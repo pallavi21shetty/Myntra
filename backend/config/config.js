e296d9c68b3904dbf664308813919b1ca77be056
pallavi21shetty <pallavishetty473@gmail.com>
2024-08-13 17:36:34 +0530

    updated

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     JWT_SECRET: '87sdhg90',
     MONGO_URI: 'mongodb://localhost:27017/user-auth',
     EMAIL_SERVICE: 'gmail',
-    EMAIL_USER: 'itd.bharathcy@gmail.com',
-    EMAIL_PASS: 'ItsEasy!1'
+    EMAIL_USER: '',
+    EMAIL_PASS: ''
   };
   
